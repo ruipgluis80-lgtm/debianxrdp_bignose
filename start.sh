08156ad35ac00ae69546c97e60c348ab769c67b8
ruipgluis80-lgtm <ruipgluis80@gmail.com>
2026-07-22 23:07:37 +0100

    Update start.sh to use PipeWire instead of PulseAudio
    Added commands to start PipeWire and its components, replacing PulseAudio.

--- start.sh
+++ start.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+# Inicia o DBus
 service dbus start
-pulseaudio --start --system --disallow-exit --disable-shm
+
+# Inicia o PipeWire (substituto do PulseAudio no Debian 12)
+pipewire &
+pipewire-pulse &
+wireplumber &
+
+# Aguarda o PipeWire iniciar completamente
+sleep 2
+
+# Inicia o XRDP
 service xrdp start
 
+# Cria diretório do X11
 mkdir -p /tmp/.X11-unix
 chmod 1777 /tmp/.X11-unix
 
+# Mantém o container rodando e mostra logs
 tail -f /var/log/xrdp-sesman.log
